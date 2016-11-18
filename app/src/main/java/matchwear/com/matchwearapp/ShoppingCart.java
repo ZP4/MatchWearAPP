dcf09e3cf8e197aca29e125884473742f33621b9
ZP4 <jasonhuzheming9@gmail.com>
2016-11-17 19:47:10 -0500

    Unable to add item storage
    Derek shopping cart (WIP) PART 1

--- app/src/main/java/matchwear/com/matchwearapp/ShoppingCart.java
+++ app/src/main/java/matchwear/com/matchwearapp/ShoppingCart.java
@@ -1,11 +1,19 @@
 package matchwear.com.matchwearapp;
 
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.app.ListActivity;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -23,7 +31,23 @@ public class ShoppingCart extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shopping_cart, container, false);
+        View view = inflater.inflate(R.layout.fragment_shopping_cart, container, false);
+
+                ListView listView = (ListView) view.findViewById(R.id.ShoppingList);
+        String items [] = {"item1", "item2", "item3"};
+        List<String> output = new ArrayList<String>();
+        output.add("hello");
+        listView.setAdapter(new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, output));
+
+
+
+
+
+
+
+
+
+        return view;
     }
 
 }

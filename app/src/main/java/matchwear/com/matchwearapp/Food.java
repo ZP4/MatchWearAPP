87e15a1285061b3359673391e7aeef373b1640d6
Unknown <yanganthony212@gmail.com>
2016-12-05 10:14:18 -0500

    finish american section of menu

--- app/src/main/java/matchwear/com/matchwearapp/Food.java
+++ app/src/main/java/matchwear/com/matchwearapp/Food.java
@@ -50,6 +50,44 @@ public HashMap<String, HashMap> American= new HashMap<String, HashMap>();
         PCT.put("Price", "$34.99");
         PCT.put("Servings", "15 pieces");
         PCT.put("ID", "USE03");
+        HashMap<String, HashMap>ASD     = new HashMap<String, HashMap>();
+        HashMap<String, String>MP       = new HashMap<String, String>();
+        MP.put("Title", "Mashed Potatoes");
+        MP.put("Description","Buttery mashed potatoes that'll remind you of home ");
+        MP.put("Price","$24.99");
+        MP.put("Servings", "1 tray");
+        MP.put("ID","USS01");
+        HashMap<String, String>PS       = new HashMap<String, String>();
+        PS.put("Title", "Potato Salad");
+        PS.put("Description","Boiled potatoes dressed with olive oil, vinegar, finely minced parsley and garlic, hot chili peppers and salt");
+        PS.put("Price","$20.99");
+        PS.put("Servings", "1 large bowl");
+        PS.put("ID","USS02");
+        HashMap<String,String>SS        = new HashMap<String, String>();
+        SS.put("Title", "Side Salad");
+        SS.put("Description", "Salad included with croutons, shredded carrots, cucumbers and tomatoes served with choice of dressing");
+        SS.put("Price", "$19.99");
+        SS.put("Servings", "1 large bowl");
+        SS.put("ID", "USS03");
+        HashMap<String, HashMap>AD      = new HashMap<String, HashMap>();
+        HashMap<String, String>AP       = new HashMap<String, String>();
+        AP.put("Title", "Apple Pie");
+        AP.put("Description", "fresh apples, mixed with the perfect blend of brown sugar and cinnamon");
+        AP.put("Price", "$32.99");
+        AP.put("Servings", "10 pieces");
+        AP.put("ID", "USD01");
+        HashMap<String, String>IC       = new HashMap<String, String>();
+        IC.put("Title", "Ice Cream");
+        IC.put("Description", "Comes in a variety of flavors; coconut, vanilla, lime , avocado, peanut, banana, and chocolate");
+        IC.put("Price", "$24.99");
+        IC.put("Servings", "10 bowls");
+        IC.put("ID", "USD02");
+        HashMap<String, String>RVC      = new HashMap<String, String>();
+        RVC.put("Title", "Red Velvet Cupcakes");
+        RVC.put("Description", "Fluffy, moist cupcakes with tangy cream cheese frosting heaped on top");
+        RVC.put("Price", "$34.99");
+        RVC.put("Servings", "10 cupcakes");
+        RVC.put("ID", "USD03");
 
 
 
@@ -66,6 +104,14 @@ public HashMap<String, HashMap> American= new HashMap<String, HashMap>();
         AE.put("Carved Double Roasted Chicken", CDRC);
         AE.put("Paremsan Crusted Ttilapia", PCT);
         American.put("Entrees", AE);
+        ASD.put("Mashed Potatoes",MP);
+        ASD.put("Potato Salad",PS);
+        ASD.put("Side Salad",SS);
+        American.put("Side Dish",ASD);
+        AD.put("Apple Pie", AP);
+        AD.put("Ice Cream", IC);
+        AD.put("Red Velvet Cupcakes", RVC);
+        American.put("Desserts", AD);
 
     }
 }
